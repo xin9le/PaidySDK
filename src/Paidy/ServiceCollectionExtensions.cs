08e4692dea150f12457d575d288a6f74918b8b42
Takaaki Suzuki <xin9le@live.jp>
2021-01-14 23:45:59 +0900

    Add PaymentService

--- src/Paidy/ServiceCollectionExtensions.cs
+++ src/Paidy/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Paidy.Payments;
 
 
 
@@ -25,7 +29,16 @@ namespace Paidy
                 if (options.ApiVersion is not null)
                     client.DefaultRequestHeaders.Add("Paidy-Version", options.ApiVersion);
             });
+            services.TryAddSingleton(static x => new PaymentService(getHttpClient(x)));
             return services;
+
+            #region Local functions
+            static HttpClient getHttpClient(IServiceProvider provider)
+            {
+                var factory = provider.GetRequiredService<IHttpClientFactory>();
+                return factory.CreateClient(httpClientName);
+            }
+            #endregion
         }
     }
 }
