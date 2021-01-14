91295b33f7df5c6072268db53fb5874f82774983
Takaaki Suzuki <xin9le@live.jp>
2021-01-14 19:56:39 +0900

    Add HttpClient

--- src/Paidy/ServiceCollectionExtensions.cs
+++ src/Paidy/ServiceCollectionExtensions.cs
@@ -17,6 +17,13 @@ namespace Paidy
         /// <returns></returns>
         public static IServiceCollection AddPaidy(this IServiceCollection services, PaidyOptions options)
         {
+            const string httpClientName = "___Paidy.HttpClient___";
+            services.AddHttpClient(httpClientName, client =>
+            {
+                client.BaseAddress = new(options.ApiEndpoint);
+                client.DefaultRequestHeaders.Authorization = new("Bearer", options.SecretKey);
+                client.DefaultRequestHeaders.Add("Paidy-Version", options.ApiVersion);
+            });
             return services;
         }
     }
