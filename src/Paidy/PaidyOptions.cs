25dd671f13ce058c730d6cc74b6f63c2702fffbd
Takaaki Suzuki <xin9le@live.jp>
2021-01-14 19:28:09 +0900

    Add paidy options

--- src/Paidy/PaidyOptions.cs
+++ src/Paidy/PaidyOptions.cs
@@ -6,15 +6,47 @@
     public sealed class PaidyOptions
     {
         #region Properties
+        /// <summary>
+        /// Gets the payment API endpoint.
+        /// </summary>
+        /// <remarks>https://api.paidy.com</remarks>
+        public string ApiEndpoint { get; private init; }
+
+
+        /// <summary>
+        /// Gets the API secret key.
+        /// </summary>
+        /// <remarks>sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxx</remarks>
+        public string SecretKey { get; private init; }
+
+
+        /// <summary>
+        /// Gets the API version.
+        /// </summary>
+        /// <remarks>2018-04-10</remarks>
+        public string ApiVersion { get; private init; }
         #endregion
 
 
         #region Constructors
+#pragma warning disable CS8618
         /// <summary>
         /// Creates instance.
         /// </summary>
         public PaidyOptions()
         { }
+#pragma warning restore CS8618
+
+
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        public PaidyOptions(string apiEndpoint, string secretKey, string apiVersion)
+        {
+            this.ApiEndpoint = apiEndpoint;
+            this.SecretKey = secretKey;
+            this.ApiVersion = apiVersion;
+        }
         #endregion
     }
 }
