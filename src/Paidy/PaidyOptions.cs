a71a8a7de6e048ece48ad992ac8f60be2524d991
Takaaki Suzuki <xin9le@live.jp>
2021-01-14 18:27:45 +0900

    Add ServiceCollectionExtensions

---
+++ src/Paidy/PaidyOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace Paidy
+{
+    /// <summary>
+    /// Represents configuration options for Paidy API.
+    /// </summary>
+    public sealed class PaidyOptions
+    {
+        #region Properties
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        public PaidyOptions()
+        { }
+        #endregion
+    }
+}
---
+++ src/Paidy/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+
+namespace Paidy
+{
+    /// <summary>
+    /// Provides <see cref="IServiceCollection"/> extension functions.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add Paidy services to DI.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddPaidy(this IServiceCollection services, PaidyOptions options)
+        {
+            return services;
+        }
+    }
+}
