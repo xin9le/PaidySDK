77d5f80e791c42f35ba792a796259f9ae9ed5019
Takaaki Suzuki <xin9le@live.jp>
2021-02-15 16:46:55 +0900

    Added useful extension methods related to HttpClient using Utf8Json.

---
+++ src/Paidy/Internals/HttpClientExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Utf8Json;
+
+
+
+namespace Paidy.Internals
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="HttpClient"/>.
+    /// </summary>
+    internal static class HttpClientExtensions
+    {
+        /// <summary>
+        /// Send the specified instance as JSON via POST method.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="client"></param>
+        /// <param name="requestUri"></param>
+        /// <param name="data"></param>
+        /// <param name="resolver"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async ValueTask<HttpResponseMessage> PostAsJsonAsync<T>(this HttpClient client, string requestUri, T data, IJsonFormatterResolver? resolver = default, CancellationToken cancellationToken = default)
+        {
+            var json = JsonSerializer.Serialize(data, resolver);
+            using (var content = new ByteArrayContent(json))
+            {
+                content.Headers.ContentType = new("application/json");
+                return await client.PostAsync(requestUri, content, cancellationToken).ConfigureAwait(false);
+            }
+        }
+
+
+        /// <summary>
+        /// Send the specified instance as JSON via PATCH method.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="client"></param>
+        /// <param name="requestUri"></param>
+        /// <param name="data"></param>
+        /// <param name="resolver"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async ValueTask<HttpResponseMessage> PatchAsJsonAsync<T>(this HttpClient client, string requestUri, T data, IJsonFormatterResolver? resolver = default, CancellationToken cancellationToken = default)
+        {
+            var json = JsonSerializer.Serialize(data, resolver);
+            using (var content = new ByteArrayContent(json))
+            {
+                content.Headers.ContentType = new("application/json");
+                return await client.PatchAsync(requestUri, content, cancellationToken).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ src/Paidy/Internals/HttpContentExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Utf8Json;
+
+
+
+namespace Paidy.Internals
+{
+    /// <summary>
+    /// Provides extension methods for <see cref="HttpContent"/>.
+    /// </summary>
+    internal static class HttpContentExtensions
+    {
+        /// <summary>
+        /// Parses as JSON and deserializes to the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="content"></param>
+        /// <param name="resolver"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async ValueTask<T> ReadFromJsonAsync<T>(this HttpContent content, IJsonFormatterResolver? resolver = default, CancellationToken cancellationToken = default)
+        {
+#if NETSTANDARD
+            var payload = await content.ReadAsByteArrayAsync().ConfigureAwait(false);
+#else
+            var payload = await content.ReadAsByteArrayAsync(cancellationToken).ConfigureAwait(false);
+#endif
+            return JsonSerializer.Deserialize<T>(payload, resolver);
+        }
+    }
+}
