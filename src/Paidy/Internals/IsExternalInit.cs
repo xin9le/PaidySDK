c95c0bd9323fcce2720f6662dbd96a5ad065a495
Takaaki Suzuki <xin9le@live.jp>
2021-02-15 16:44:58 +0900

    The IsExternalInit class was added to allow the init accessor to be used for .NET Standard environment.

---
+++ src/Paidy/Internals/IsExternalInit.cs
@@ -0,0 +1,10 @@
+﻿#if NETSTANDARD
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Runtime.CompilerServices
+{
+    internal sealed class IsExternalInit
+    { }
+}
+#endif
