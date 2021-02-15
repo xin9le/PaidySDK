ac4dd0cb3f14e52b85a798e765ab81acc87bf2b8
Takaaki Suzuki <xin9le@live.jp>
2021-02-15 18:10:53 +0900

    Add error response

---
+++ src/Paidy/Payments/Entities/ErrorResponse.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Runtime.Serialization;
+
+
+
+namespace Paidy.Payments.Entities
+{
+    /// <summary>
+    /// Represents error object.
+    /// </summary>
+    public sealed class ErrorResponse
+    {
+#pragma warning disable CS8618
+        /// <summary>
+        /// Internal error code.
+        /// </summary>
+        [DataMember(Name = "reference")]
+        public string Reference { get; private init; }
+
+
+        /// <summary>
+        /// HTTP response code.
+        /// </summary>
+        [DataMember(Name = "status")]
+        public HttpStatusCode Status { get; private init; }
+
+
+        /// <summary>
+        /// Code indicating the kind of error that occurred.
+        /// </summary>
+        [DataMember(Name = "code")]
+        public string Code { get; private init; }
+
+
+        /// <summary>
+        /// Text version of the error code.
+        /// This field can sometimes provide the name of the field with which there is an issue.
+        /// </summary>
+        [DataMember(Name = "title")]
+        public string Title { get; private init; }
+
+
+        /// <summary>
+        /// Description providing more details about the error.
+        /// </summary>
+        [DataMember(Name = "description")]
+        public string Description { get; private init; }
+#pragma warning restore CS8618
+    }
+}
---
+++ src/Paidy/Payments/PaymentException.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net;
+using Paidy.Payments.Entities;
+using Utf8Json;
+using Utf8Json.Resolvers;
+
+
+
+namespace Paidy.Payments
+{
+    /// <summary>
+    /// Represents an exception occurred by the Paidy API.
+    /// </summary>
+    public sealed class PaymentException : Exception
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the HTTP status code.
+        /// </summary>
+        public HttpStatusCode StatusCode { get; }
+
+
+        /// <summary>
+        /// Gets the raw error information.
+        /// </summary>
+        public string RawError { get; }
+
+
+        /// <summary>
+        /// Gets the error information.
+        /// </summary>
+        public ErrorResponse ErrorResponse
+            => this.errorResponse
+            ??= JsonSerializer.Deserialize<ErrorResponse>(this.RawError, StandardResolver.AllowPrivate);
+        private ErrorResponse? errorResponse;
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="statusCode"></param>
+        /// <param name="rawError"></param>
+        internal PaymentException(HttpStatusCode statusCode, string rawError)
+            : this("An exception was occured while communicating with the Paidy API.", statusCode, rawError)
+        { }
+
+
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="statusCode"></param>
+        /// <param name="rawError"></param>
+        internal PaymentException(string message, HttpStatusCode statusCode, string rawError)
+            : this(message, null, statusCode, rawError)
+        { }
+
+
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        /// <param name="statusCode"></param>
+        /// <param name="rawError"></param>
+        internal PaymentException(string message, Exception? innerException, HttpStatusCode statusCode, string rawError)
+            : base(message, innerException)
+        {
+            this.StatusCode = statusCode;
+            this.RawError = rawError;
+        }
+        #endregion
+    }
+}
