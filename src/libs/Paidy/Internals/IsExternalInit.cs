315a97b6d87dd9560e09febb1883dfe3b10958e4
Takaaki Suzuki <xin9le@live.jp>
2025-11-18 15:21:20 +0900

    Suppress warning

--- src/libs/Paidy/Internals/IsExternalInit.cs
+++ src/libs/Paidy/Internals/IsExternalInit.cs
@@ -2,7 +2,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#pragma warning disable IDE0130 
 namespace System.Runtime.CompilerServices;
+#pragma warning restore IDE0130
 
 internal sealed class IsExternalInit
 { }
