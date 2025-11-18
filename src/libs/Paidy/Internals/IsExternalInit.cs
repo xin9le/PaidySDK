5273dbcc0640f035e098fc3142a02c09f7f5211b
Takaaki Suzuki <xin9le@live.jp>
2025-11-18 15:20:46 +0900

    Fix targeting framework

--- src/libs/Paidy/Internals/IsExternalInit.cs
+++ src/libs/Paidy/Internals/IsExternalInit.cs
@@ -1,4 +1,4 @@
-﻿#if NETSTANDARD || NET461
+﻿#if NETSTANDARD || NET462
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
